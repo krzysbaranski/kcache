6609f02127de361934ed0f837f40f75982cae2c6
Krzysztof Baranski <baranski5@gmail.com>
2020-10-18 13:26:11 +0200

    readme and setup.py

---
+++ README.md
@@ -0,0 +1,8 @@
+# kcache #
+
+Cache library for Python
+
+It can be used for simple caching or object lifecycle management
+
+- Source code: https://github.com/krzysbaranski/kcache/
+- Python PyPi repository: https://pypi.org/project/kcache/
--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 REQUIREMENTS = {
     'base': [],
@@ -7,15 +9,23 @@ REQUIREMENTS = {
 }
 setup(
     name='kcache',
-    version='0.0.2',
+    version='0.0.3',
     package_dir={'': 'src'},
     packages=['kcache'],
     url='https://github.com/krzysbaranski/kcache',
     license='GNU Lesser General Public License Version 3',
     author='Krzysztof Baranski',
     author_email='baranski5@gmail.com',
-    description='Cache',
+    description='Cache Library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=REQUIREMENTS['base'],
     tests_require=REQUIREMENTS['test'],
-    extras_require={'test': REQUIREMENTS['test']}
+    extras_require={'test': REQUIREMENTS['test']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+        "Typing :: Typed"
+    ],
+    python_requires='>=3.7',
 )
